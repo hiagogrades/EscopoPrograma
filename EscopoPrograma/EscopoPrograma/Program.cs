8934c11ed580364d7b6249563cbb82bf9ff46973
Hiago Grades <hiagogrades@hotmail.com>
2022-05-22 04:34:18 -0300

    Fist commit

---
+++ EscopoPrograma/EscopoPrograma/Program.cs
@@ -0,0 +1,18 @@
+﻿//Estrutura do C#
+
+//Importações
+using System;
+
+//Divisões lógicas
+namespace EscopoPrograma
+{
+    //Programa
+    internal class Program
+    {
+        //Método principal (Sempre será executado primeiro)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
